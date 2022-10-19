5622f0ab332ab397054edd1807f3e03095585aec
1550282237 <1550282237@qq.com>
2022-10-19 15:18:04 +0800

    stach

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,15 +2,22 @@ import styles from './index.less';
 
 export default function IndexPage() {
 
- 
-
 
   return (
     <div>
       <h1 className={styles.title}>Page index</h1>
+      <h1 className={styles.title}>这是 2 号标题</h1>
       <h1 className={styles.title}>Page index</h1>
       <h1 className={styles.title}>Page index</h1>
       <h2 className={styles.title}>Page index</h2>
+
+
+      <h3> 这个一个好的代码！！  </h3>
+
+
+
     </div>
   );
+
+
 }
